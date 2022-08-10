3687e182b6424190fbee0abadbb8f307fc2bdf03
Chandrasekhara Reddy Desi Reddy Kasivaraprasada <chandrasekhara.desireddy@hallmarkhcs.com>
2022-08-10 20:30:45 +0530

    Form2

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-form2',
   templateUrl: './form2.component.html',
@@ -7,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form2Component implements OnInit {
 
+  contactForm = new FormGroup(
+    {
+     firstname:new FormControl(),
+     lastname:new FormControl(),
+     email:new FormControl(), 
+     gender:new FormControl(),
+     isMarried:new FormControl(),
+     country:new FormControl()
+    }
+  )
+
+  onSUbmit(){
+    console.log("Inside Submit",this.contactForm.value)
+  }
   constructor() { }
 
   ngOnInit(): void {
